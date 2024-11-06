551145e63831c3f130b3ebc0932498a70e65595f
GunaNalam <nalamguna15@gmail.com>
2024-11-06 18:24:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,32 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { AudioRecorder } from 'react-audio-voice-recorder';
 
 function AudioRecorder1() {
-  const [audioUrl, setAudioUrl] = useState(null);
-
-  const handleRecordingComplete = async (blob) => {
-    // Assuming the Flask backend is running on http://127.0.0.1:5000
-    const formData = new FormData();
-    formData.append('audio', blob);
-
-    try {
-      const response = await fetch('http://127.0.0.1:5000/upload-audio', {
-        method: 'POST',
-        body: formData,
-      });
-
-      const data = await response.json();
-      setAudioUrl(data.audioUrl);
-    } catch (error) {
-      console.error('Error uploading audio:', error);
+  let b=0;
+  const addAudioElement = (blob) => {
+    if (!b) {
+      const url = URL.createObjectURL(blob);
+      const audio = document.createElement("audio");
+      audio.src = url;
+      audio.controls = true;
+      document.body.appendChild(audio);
+      b=1;
     }
-  };
+    else document.body.getElementsByTagName('audio').src=URL.createObjectURL(blob);
+  }
 
   return (
     <>
       <div>Sound Recorder for Emotion Detection</div>
-      <AudioRecorder
-        onRecordingComplete={handleRecordingComplete}
-        audioTrackConstraints={{
-          noiseSuppression: true,
-          echoCancellation: true,
-        }}
-        downloadOnSavePress={false}
-        downloadFileExtension="webm"
-      />
-      {audioUrl && <div>Audio URL: <a href={audioUrl}>{audioUrl}</a></div>}
+      <AudioRecorder 
+      onRecordingComplete={addAudioElement}
+      audioTrackConstraints={{
+        noiseSuppression: true,
+        echoCancellation: true,
+      }} 
+      downloadOnSavePress={false}
+      downloadFileExtension="webm"/>
     </>
   );
 }
